f2d71139393a0b544908daba69e36e457160013b
alsrb0332@naver.com <alsrb0332@naver,com>
2025-04-09 23:34:57 +0900

    Feat: initial commit

---
+++ src/main/java/com/softwareengineering9/toeicVoca/ToeicVocaApplication.java
@@ -0,0 +1,13 @@
+package com.softwareengineering9.toeicVoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicVocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicVocaApplication.class, args);
+	}
+
+}
